53120f027a9da7622feaa9d1793cabcaff0e3f29
leemurking <85164123+leemurking@users.noreply.github.com>
2024-08-19 20:56:47 +0300

    Update Dashapp.py

--- Dashapp.py
+++ Dashapp.py
@@ -84,7 +84,7 @@ gender_smoker_box = px.box(medical, x='sex', y='medical charges', color='smoker'
 
 # Initialize the Dash app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
+server = app.server
 # Apply custom CSS
 app.index_string = '''
 <!DOCTYPE html>
